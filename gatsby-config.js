f2d460ffa7fefddbc5cc4331ebd6e119ae2ad7dc
ijayasurya <67519756+ijayasurya@users.noreply.github.com>
2020-10-17 23:18:18 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,3 @@
-siteMetadata {
-  title: 'Gatsby',
-  description: 'A static site generator',
-  siteUrl: 'https://gatsbyjs.org',
-  author: 'Author Name'
-},
-  
 require(`dotenv`).config()
 
 module.exports = {
@@ -12,60 +5,6 @@ module.exports = {
     title: "Reflex",
     description: "Starter for Reflex.",
     siteUrl: process.env.SITE_URL || "http://localhost:8000",
-  },
-  plugins: [
-  {
-    resolve: 'gatsby-plugin-feed-generator',
-    options: {
-    generator: `GatsbyJS`,
-    rss: true, // Set to true to enable rss generation
-    json: true, // Set to true to enable json feed generation
-    siteQuery: `
-      {
-        site {
-          siteMetadata {
-            title
-            description
-            siteUrl
-            author
-          }
-        }
-      }
-    `,
-    feeds: [
-      {
-        name: 'feed', // This determines the name of your feed file => feed.json & feed.xml
-        query: `
-        {
-          allMarkdownRemark(
-            sort: {order: DESC, fields: [frontmatter___date]},
-            limit: 100,
-            ) {
-            edges {
-              node {
-                html
-                frontmatter {
-                  date
-                  path
-                  title
-                }
-              }
-            }
-          }
-        }
-        `,
-        normalize: ({ query: { site, allMarkdownRemark } }) => {
-          return allMarkdownRemark.edges.map(edge => {
-            return {
-              title: edge.node.frontmatter.title,
-              date: edge.node.frontmatter.date,
-              url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
-              html: edge.node.html,
-            }
-          })
-        },
-      },
-    ],
   },
   plugins: [
     "@reflexjs/gatsby-theme-base",
