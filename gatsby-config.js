52653ea5221ff9921a82e07ef608b1dbd6646290
ijayasurya <67519756+ijayasurya@users.noreply.github.com>
2020-10-17 23:16:16 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,60 @@ module.exports = {
     siteUrl: process.env.SITE_URL || "http://localhost:8000",
   },
   plugins: [
+  {
+    resolve: 'gatsby-plugin-feed-generator',
+    options: {
+    generator: `GatsbyJS`,
+    rss: true, // Set to true to enable rss generation
+    json: true, // Set to true to enable json feed generation
+    siteQuery: `
+      {
+        site {
+          siteMetadata {
+            title
+            description
+            siteUrl
+            author
+          }
+        }
+      }
+    `,
+    feeds: [
+      {
+        name: 'feed', // This determines the name of your feed file => feed.json & feed.xml
+        query: `
+        {
+          allMarkdownRemark(
+            sort: {order: DESC, fields: [frontmatter___date]},
+            limit: 100,
+            ) {
+            edges {
+              node {
+                html
+                frontmatter {
+                  date
+                  path
+                  title
+                }
+              }
+            }
+          }
+        }
+        `,
+        normalize: ({ query: { site, allMarkdownRemark } }) => {
+          return allMarkdownRemark.edges.map(edge => {
+            return {
+              title: edge.node.frontmatter.title,
+              date: edge.node.frontmatter.date,
+              url: site.siteMetadata.siteUrl + edge.node.frontmatter.path,
+              html: edge.node.html,
+            }
+          })
+        },
+      },
+    ],
+  },
+  plugins: [
     "@reflexjs/gatsby-theme-base",
     "@reflexjs/gatsby-theme-post",
     {
