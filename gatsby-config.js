dde61bfd1c9646cd6cf2befdf5dbda0716c52b4a
ijayasurya <67519756+ijayasurya@users.noreply.github.com>
2020-10-18 09:55:24 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,61 @@ module.exports = {
   plugins: [
     "gatsby-plugin-advanced-sitemap",
     "@reflexjs/gatsby-theme-base",
+    "gatsby-plugin-feed",
     "@reflexjs/gatsby-theme-post",
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Your Site's RSS Feed",
+          },
+        ],
+      }
+    },
     {
       resolve: "@reflexjs/gatsby-plugin-metatags",
       options: {
