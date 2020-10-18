dddfcf8380bc26ed15f22a895a1b82d3b5f4251f
ijayasurya <67519756+ijayasurya@users.noreply.github.com>
2020-10-18 10:03:20 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,48 +22,49 @@ module.exports = {
                 title
                 description
                 siteUrl
-                site_url: siteUrl
               }
             }
           }
         `,
         feeds: [
           {
-            serialize: ({ query: { site, allMarkdownRemark } }) => {
-              return allMarkdownRemark.edges.map(edge => {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map(edge => {
                 return Object.assign({}, edge.node.frontmatter, {
                   description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
+                  data: edge.node.frontmatter.date,
                   url: site.siteMetadata.siteUrl + edge.node.fields.slug,
                   guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": edge.node.html }],
-                })
-              })
+                  custom_elements: [{ 'content:encoded': edge.node.html }],
+                });
+              });
             },
             query: `
-              {
-                allMarkdownRemark(
-                  sort: { order: DESC, fields: [frontmatter___date] },
-                ) {
-                  edges {
-                    node {
-                      excerpt
-                      html
-                      fields { slug }
-                      frontmatter {
-                        title
-                        date
-                      }
+            {
+              allMdx(
+                limit: 1000,
+                sort: { order: DESC, fields: [frontmatter___date] },
+                filter: { frontmatter: { published: { ne: false } } }
+              ) {
+                edges {
+                  node {
+                    fields { slug }
+                    frontmatter {
+                      title
+                      date
                     }
+                    html
                   }
                 }
               }
+            }
             `,
-            output: "/rss.xml",
-            title: "Your Site's RSS Feed",
+            output: '/rss.xml',
+            title: `Mindless - Agney's Technical Blog RSS Feed`,
+            site_url: `https://ijayasurya.netlify.app/`,
           },
         ],
-      }
+      },
     },
     {
       resolve: "@reflexjs/gatsby-plugin-metatags",
